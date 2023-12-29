94f3fd93d0cb9469e0adf44db5df83f1471205d0
suman kumar <sumankumar21041999@gmail.com>
2023-12-29 16:25:11 +0530

    initial file upload

--- index.js
+++ index.js
@@ -17,13 +17,10 @@ app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
 
-//app.use('/posts', postRoutes);
-//app.use('/user', userRoutes);
+app.use('/posts', postRoutes);
+app.use('/user', userRoutes);
 
-app.use("/", (req, res)=>{
-  res.json({ message: "hello from Server"});
-}
-);
+
 
 
 const CONNECTION_URL = "mongodb+srv://sumankumar21041999:social123@cluster1.vigicqk.mongodb.net/?retryWrites=true&w=majority";
