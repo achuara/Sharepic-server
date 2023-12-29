14de0d1d56feb9fc80182af37f353f0c135fb910
suman kumar <sumankumar21041999@gmail.com>
2023-12-29 14:31:35 +0530

    removed code to test server

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
 
-app.use('/posts', postRoutes);
-app.use('/user', userRoutes);
+//app.use('/posts', postRoutes);
+//app.use('/user', userRoutes);
 
 app.use("/", (req, res)=>{
   res.json({ message: "hello from Server"});
